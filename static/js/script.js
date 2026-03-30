189b511c925227f86b429647f263c39bd6df88e7
Karm0101 <karmtawfiq@gmail.com>
2026-03-30 22:55:48 +0100

    Fixed verification for login page

--- static/js/script.js
+++ static/js/script.js
@@ -48,37 +48,32 @@ function login_validation() {
     let inputted_username = username_box.value
     let inputted_password = password_box.value
     const error_box = document.getElementById("error_box")
-    if(validateUsername(inputted_username) || validatePassword(inputted_password)) {
-        // Output error message(s)
-        error_box.innerHTML = `${validateUsername(inputted_username)}<br></br>${validatePassword(inputted_password)}`
-    }
-    else {
-        // Store data to be passed through to backend
-        const formData = new FormData()
-        formData.append('username', inputted_username)
-        formData.append('password', inputted_password)
-        // Backend route is specified
-        fetch('/verify_user', {
-            method: 'POST',
-            body: formData
-        })
-        .then(response => {
-            if(response.ok) {
-                return response.json()
-            }
-        })
-        .then(data => {
-            if(data.length == 1) {
-                // If the user has successfully logged in, they are redirected to the home page
-                window.location = data[0]
-            }
-            else {
-                // Error message(s) displayed
-                error_box.textContent = `${data[0]}`
-            }
-        })
-        .catch(error => error_box.textContent = `${error}`)
-    }
+
+    // Store data to be passed through to backend
+    const formData = new FormData()
+    formData.append('username', inputted_username)
+    formData.append('password', inputted_password)
+    // Backend route is specified
+    fetch('/verify_user', {
+        method: 'POST',
+        body: formData
+    })
+    .then(response => {
+        if(response.ok) {
+            return response.json()
+        }
+    })
+    .then(data => {
+        if(data.length == 1) {
+            // If the user has successfully logged in, they are redirected to the home page
+            window.location = data[0]
+        }
+        else {
+            // Error message(s) displayed
+            error_box.textContent = `${data[0]}`
+        }
+    })
+    .catch(error => error_box.textContent = `${error}`)
     // Resets value entered in username and password box as the page does not reload upon submission
     username_box.value = ''
     password_box.value = ''
@@ -116,7 +111,7 @@ function validatePassword(password) {
         return "Password must not be empty or just whitespace"
     }
     else if(password.length < 12) {
-        return "password is too short"
+        return "Password is too short"
     }
     else if(password == password.toLowerCase()) {
         return "Password must contain at least one capital letter"
